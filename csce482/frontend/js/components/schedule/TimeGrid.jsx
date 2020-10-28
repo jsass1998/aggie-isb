69d00d8cc903a8a5e238a86a1a68cf0623844c96
Benjamin Lane <benjaminlane97@gmail.com>
2020-10-28 09:27:50 -0500

    Remove console log

--- csce482/frontend/js/components/schedule/TimeGrid.jsx
+++ csce482/frontend/js/components/schedule/TimeGrid.jsx
@@ -34,7 +34,7 @@ function TimeGrid() {
           style={{ width: "100%", height: "100%" }}
           originDate={new Date('2019-03-04')}
           schedule={schedule}
-          onChange={params => {console.log(params); setSchedule(params)}}
+          onChange={params => {setSchedule(params)}}
           visualGridVerticalPrecision={15} // show grid lines in 'x' minute intervals
           verticalPrecision={5} // Minute increments in which time blocks can be created
           cellClickPrecision={60} // Size of time block in minutes when user simply clicks once on grid
