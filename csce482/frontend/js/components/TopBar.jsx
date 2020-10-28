8f6853552f1bc75a496ef8cb7cf0d41c81ac3b7a
Benjamin Lane <benjaminlane97@gmail.com>
2020-10-27 22:46:01 -0500

    Add Google auth component to front end

--- csce482/frontend/js/components/TopBar.jsx
+++ csce482/frontend/js/components/TopBar.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import GoogleLogin from "react-google-login";
+import { GOOGLE_CLIENT_ID } from "../utils/constants";
 
 class TopBar extends Component {
     constructor(props) {
@@ -6,14 +8,21 @@ class TopBar extends Component {
     }
 
     render() {
+        const googleResponse = (response) => {
+          console.log(response);
+        }
         return(
             <div className='aisb-topbar aisb-card'>
                 <h1>Aggie ISB</h1>
                 {/* TODO: WIP - Finish user acct / sign in component */}
-                {/*<div id='sign-in'>*/}
-                {/*    <p>Sign in</p>*/}
-                {/*    <span class='dot'></span>*/}
-                {/*</div>*/}
+                <div id='sign-in'>
+                    <GoogleLogin
+                      clientId={GOOGLE_CLIENT_ID}
+                      buttonText="Sign in"
+                      onSuccess={googleResponse}
+                      onFailure={googleResponse}
+                    />
+                </div>
             </div>
         );
     }
