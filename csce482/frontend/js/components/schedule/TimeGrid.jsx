1c6aee56cdfc182bacc5770d64572b3867e9e37c
Benjamin Lane <benjaminlane97@gmail.com>
2020-10-27 22:48:21 -0500

    Add TimeGrid interactivity as functional component

--- csce482/frontend/js/components/schedule/TimeGrid.jsx
+++ csce482/frontend/js/components/schedule/TimeGrid.jsx
@@ -10,16 +10,21 @@ const rangeStrings = [
   ['2019-03-07 01:30', '2019-03-07 03:00'],
   ['2019-03-07 05:30', '2019-03-07 10:00'],
   ['2019-03-08 12:30', '2019-03-08 01:30'],
-  ['2019-03-09 22:00', '2019-03-09 23:59'],
 ];
+
+const testRange =  ['2019-03-09 22:00', '2019-03-09 23:59'];
 
 const defaultSchedule = rangeStrings.map(range =>
   range.map(dateString => new Date(dateString)),
 );
 
+const newTimeBlock = testRange.map(dateString => new Date(dateString));
+
 // TODO: Convert TimeGrid from a function to a react Component
 function TimeGrid() {
   const [schedule, setSchedule] = useState(defaultSchedule);
+  // console.log('schedule', schedule);
+  // console.log('newTimeBlock', newTimeBlock);
 
   return (
     <div>
@@ -29,12 +34,15 @@ function TimeGrid() {
           style={{ width: "100%", height: "100%" }}
           originDate={new Date('2019-03-04')}
           schedule={schedule}
-          onChange={setSchedule}
+          onChange={params => {console.log(params); setSchedule(params)}}
           visualGridVerticalPrecision={15} // show grid lines in 'x' minute intervals
-          // verticalPrecision={15} //
-          // cellClickPrecision={60}
+          verticalPrecision={5} // Minute increments in which time blocks can be created
+          cellClickPrecision={60} // Size of time block in minutes when user simply clicks once on grid
         />
       </div>
+      <button onClick={() => setSchedule([...schedule, newTimeBlock])}>
+        Add time
+      </button>
     </div>
   );
 }
