fbd38da0b6635120b2aae67c259b6a9d0ca0a636
Carlson L <carlsonblindley@tamu.edu>
2020-10-31 20:09:05 -0500

    Wrote scheduling algorithm
    Wrote scheduling algorithm. Need to get some brain food, double check it, and test it before merging any commits

--- csce482/backend/api/models.py
+++ csce482/backend/api/models.py
@@ -102,31 +102,6 @@ class Section(models.Model):
            'user'
        ).count()
 
-class Schedule(models.Model):
-    #Many Schedules can be associated with a User
-    user = models.ForeignKey(
-        'users.User',
-        on_delete=models.CASCADE
-    )
-    #A schedule can have many Activities
-    #An Activity can be part of many Schedules
-    activities = models.ManyToManyField(Activity)
-    #term = models.CharField(max_length=20)
-
-    def __str__(self):
-        s=str(self.id)+": "+str(self.user)
-        s=s+" ("+str(self.activities.count())+" activities)"
-        return s
-    
-    def conflicts_with(self, activity):
-        for a in self.activities:
-            if a.conflicts_with(activity):
-                return True
-        return False
-
-    def get_sections(self):
-        return self.activities.all().filter(section__isnull = False)
-
 class Activity_Instance(models.Model):
     activity = models.ForeignKey(
         Activity,
@@ -151,3 +126,65 @@ class Activity_Instance(models.Model):
         b = b and self.starttime < other_instance.endtime
         b = b and self.endtime > other_instance.starttime
         return b
+
+
+class Schedule(models.Model):
+    #Many Schedules can be associated with a User
+    user = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE
+    )
+    #A schedule can have many Activities
+    #An Activity can be part of many Schedules
+    activities = models.ManyToManyField(Activity)
+    term = models.CharField(max_length=20)
+
+    def __str__(self):
+        s=str(self.id)+": "+str(self.user)
+        s=s+" ("+str(self.activities.count())+" activities)"
+        return s
+    
+    def conflicts_with(self, activity):
+        for a in self.activities:
+            if a.conflicts_with(activity):
+                return True
+        return False
+
+    def get_sections(self):
+        return self.activities.all().filter(section__isnull = False)
+    
+    def generate_schedules(schedule_user, selected_courses, schedule_term):
+        count = len(selected_courses)
+        section_lists = [course.get_sections(schedule_term) for course in selected_courses]
+        schedules = generate_schedules(schedule_user, schedule_term, section_lists, count-1)
+        return schedules
+    
+    def generate_schedules(schedule_user, schedule_term, section_lists, count):
+        if count == 0:
+            schedules = []
+            for section in section_lists[0]:
+                new_schedule = Schedule.objects.create(
+                    user = schedule_user,
+                    term = schedule_term
+                )
+                new_schedule.activities.set([section.activity])
+                new_schedule.save()
+                schedules.append(new_schedule)
+            return schedules
+        else:
+            schedules = generate_schedule(schedule_user, schedule_term, section_lists, count-1)
+            next_schedules = []
+            for schedule in schedules:
+                for section in section_lists[count]:
+                    if not schedule.conflicts_with(section):
+                        curr_sections = schedule.get_sections()
+                        next_schedule = Schedule.objects.create(
+                            user = schedule_user,
+                            term = schedule_term
+                        )
+                        next_schedule.activities.set(curr_sections + [section.activity])
+                        next_schedule.save()
+                        next_schedules.append(next_schedule)
+            return next_schedules
+                        
+
