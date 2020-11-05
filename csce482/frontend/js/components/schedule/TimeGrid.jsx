1a73c9462ec8b765d4dffd4104bbf19bc8bb8dac
Benjamin Lane <benjaminlane97@gmail.com>
2020-11-05 02:41:21 -0600

    Remove outdated code

--- csce482/frontend/js/components/schedule/TimeGrid.jsx
+++ csce482/frontend/js/components/schedule/TimeGrid.jsx
@@ -10,7 +10,6 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import makeStyles from "@material-ui/core/styles/makeStyles";
 import TextField from "@material-ui/core/TextField";
 
 /*
@@ -31,59 +30,20 @@ const rangeStrings = [
   // ['2019-03-08 12:30', '2019-03-08 01:30'],
 ];
 
-const testRange =  ['2019-03-09 22:00', '2019-03-09 23:59'];
-
 const defaultSchedule = rangeStrings.map(range =>
   range.map(dateString => new Date(dateString)),
 );
-
-const newTimeBlock = testRange.map(dateString => new Date(dateString));
 
 const convertToTimeBlock = rangeStrings => {
   return rangeStrings.map(range => range.map(dateString => new Date(dateString)),);
 }
 
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-}));
-
-const availableCourses = [
-    {
-      title: 'CSCE 482-900',
-      instance: [
-        ['2019-03-04 09:00', '2019-03-04 12:00'], // Monday
-        ['2019-03-06 09:00', '2019-03-06 12:00'], // Wednesday
-      ]
-    },
-    {
-      title: 'CSCE 436-500',
-      instance: [
-        ['2019-03-04 14:55', '2019-03-04 15:45'], // Monday
-        ['2019-03-06 14:55', '2019-03-06 15:45'], // Wednesday
-        ['2019-03-08 14:55', '2019-03-08 15:45'], // Friday
-      ]
-    },
-    {
-      title: 'MATH 407-500',
-      instance: [
-        ['2019-03-05 17:00', '2019-03-05 18:15'], // Tuesday
-        ['2019-03-07 17:00', '2019-03-07 18:15'], // Thursday
-      ]
-    },
-  ];
 
 function handleCheckChanged(checked) {
   localStorage.setItem('hideToolTips', checked ? '1' : '0');
 }
 
 function CourseSelectionPopUp(props) {
-  const classes = useStyles();
   const [courses, setCourses] = useState([]);
 
   const handleChange = (event, newValue) => {
