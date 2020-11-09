05d03dbb2d31d2e7f1cac870c2a3b150a9046eb8
jsass1998 <gh.bkklol@gmail.com>
2020-11-09 10:45:26 -0600

    fix

--- csce482/backend/api/management/commands/commands.py
+++ csce482/backend/api/management/commands/commands.py
@@ -4,18 +4,18 @@ from django.core.management import base
 class Command(base.BaseCommand):
 
     def add_arguments(self, parser):
-    
-        parser.add_argument('--RMP', action='store_true')
+        parser.add_argument('--OneCommand')    
         parser.add_argument('--CoursesRecent', action='store_true')
 
     def handle(self, *args, **options):
-        if options['RMP']:
-            management.call_command('UpdateProfessorRMPInfo')
-        
-        elif options['CoursesRecent']:
+        if options['CoursesRecent']:
             management.call_command('scrape_depts_rev')
             management.call_command('scrape_courses_rev')
             management.call_command('UpdateProfessorRMPInfo')
+            
+        elif options['OneCommand']:
+            command = options['OneCommand']
+            management.call_command(command)
             
         else:
             management.call_command('scrape_courses')
